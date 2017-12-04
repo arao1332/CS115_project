bb46a42950d2f1cd1a30e021ae9b145c59d0d9c4
matt899 <34108067+matt899@users.noreply.github.com>
2017-12-04 16:29:17 -0500

    Update CS_AUDIO.java

--- CS_AUDIO/src/cs_audio/CS_AUDIO.java
+++ CS_AUDIO/src/cs_audio/CS_AUDIO.java
@@ -18,7 +18,6 @@ public void fixeddelayLine(int t, double m){
 double m = 0.5;
 int t = 44100;
 //for the array: i=t; i < my_wav.length, i++ 
-double[][] delaybuffer = new double[][];
 my_wav[0][1000]+ = my_wav[0][0];
 my_wav[0][i] = my_wav[0][i] * m + my_wav[0][i-t] * (1-m)
 }
