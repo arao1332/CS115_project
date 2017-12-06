288256872638202dfcd5a0199e84b2cac91d5201
matt899 <34108067+matt899@users.noreply.github.com>
2017-12-05 19:44:42 -0500

    Update CS_AUDIO.java

--- CS_AUDIO/src/cs_audio/CS_AUDIO.java
+++ CS_AUDIO/src/cs_audio/CS_AUDIO.java
@@ -11,21 +11,20 @@ package delayLine;
  */
 public class delayLine extends WaveTools
 {
+  protected double[][] delaybuffer = new double[][];
 
-protected double[][] delaybuffer = new double[][];
-
-public void fixeddelayLine(int t, double m){
-double m = 0.5;
-int t = 44100;
+//public void fixeddelayLine(int t, double m){
+//double m = 0.5;
+//int t = 44100;
 //for the array: i=t; i < my_wav.length, i++ 
-my_wav[0][1000]+ = my_wav[0][0];
-my_wav[0][i] = my_wav[0][i] * m + my_wav[0][i-t] * (1-m)
-}
+//my_wav[0][1000]+ = my_wav[0][0];
+//my_wav[0][i] = my_wav[0][i] * m + my_wav[0][i-t] * (1-m)
+//}
 
-public void fixedDelay(int t, double m){
+public void fixedDelay(int t, double m)
+{
   for (int i = t; i < my_wav.length(); i++)
   {
     my_wav[0][i] = my_wav[0][i] * m += my_wav[0][i-t] * (1-m);
   }
-
 }
